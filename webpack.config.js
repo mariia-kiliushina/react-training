46eaa745f07c9eb95348cbeeae8de2590d5d82b0
Mariia Kiliushina <markil@Mariias-MacBook-Air.local>
2022-05-01 08:56:03 +0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  devServer: {
+    port: 3002,
+  },
+  devtool: "source-map",
+  entry: "./src/index.jsx",
+  mode: process.env.MODE,
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js(x?)$/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    sourceMapFilename: "[name].js.map",
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[file].map[query]",
+    }),
+    new webpack.EnvironmentPlugin({
+      MODE: process.env.MODE,
+    }),
+    new HTMLWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+};
